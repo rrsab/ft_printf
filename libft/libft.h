46126e9bea35fd0b1875fb31029fd15e6278e9e9
Salena Alyce <salyce@va-i5.kzn.21-school.ru>
2021-05-18 10:52:54 +0300

    add libft

---
+++ libft/libft.h
@@ -0,0 +1,14 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+int		ft_atoi(const char *str);
+int		ft_isdigit(int ch);
+char	*ft_itoa(int n);
+size_t	ft_strlen(const char *str);
+int		ft_tolower(int s1);
+char	*ft_strdup(const char *s1);
+#endif
